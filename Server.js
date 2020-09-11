b4a346fba31878037dff7bbbb402994906a76bf4
jakequicksilver9 <jakequicksilver9@gmail.com>
2020-09-11 13:53:00 -0400

    redis

--- Server.js
+++ Server.js
@@ -4,6 +4,13 @@ const mainController = require('./mainController');
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const app = express()
+const redis = require('redis')
+const redisStore = require('connect-redis')(session)
+// const client  = redis.createClient()
+
+// Add your cache name and access key.
+// const client = redis.createClient({auth_pass: 'H7bqPJhCsgNQnntepaquDh8vUpp0laaRyQTnl32u3Wc=', host: 'schuppdev.redis.cache.windows.net', port: 6380,ttl : 260});
+var client = redis.createClient(6380, 'schuppdev.redis.cache.windows.net', {auth_pass: 'H7bqPJhCsgNQnntepaquDh8vUpp0laaRyQTnl32u3Wc=', tls: {servername: 'schuppdev.redis.cache.windows.net'}})
 
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname,'css')));
@@ -13,6 +20,13 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true   
+}))
+
+app.use(session({
+    secret: 'ssshhhhh',
+    store: new redisStore({ host: 'schuppdev.redis.cache.windows.net', port: 6380, client: client,tls: {servername: 'schuppdev.redis.cache.windows.net'}}),
+    saveUninitialized: false,
+    resave: false
 }))
 
 app.use('/', mainController)
--- mainController.js
+++ mainController.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res) => {
-    
+    let sess = req.session
+    if(sess.user) {
+        return res.redirect('/admin')
+    }
     res.render('index')
-
 })
 
 module.exports = router
